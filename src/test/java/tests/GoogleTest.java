89b28d2a783cd19bf1b588a3900954a5ef2b9dac
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-11 19:57:10 -0500

    google practice code added

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -1,4 +1,26 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GoogleTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().version("79").setup();
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("http://google.com");
+        Thread.sleep(3000);
+
+        driver.quit();
+
+
+
+
+
+
+
+
+
+
+    }
 }
